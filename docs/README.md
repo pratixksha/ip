7c3a341d75ad2806c8ee4e59359a45aafcb586d4
Pratiksha <pratira06@gmail.com>
2026-08-15 12:05:49 +0800

    Rename chatbot from Duke to Shrek

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Shrek User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Shrek.java
@@ -0,0 +1,13 @@
+/**
+ * The entry point for the Shrek chatbot.
+ */
+public class Shrek {
+    public static void main(String[] args) {
+        String banner = " ____  _              _    \n"
+                + "/ ___|| |__  _ __ ___| | __\n"
+                + "\\___ \\| '_ \\| '__/ _ \\ |/ /\n"
+                + " ___) | | | | | |  __/   < \n"
+                + "|____/|_| |_|_|  \\___|_|\\_\\\n";
+        System.out.println(banner);
+    }
+}
